cc6e377aba239d79497e327b070d67a4793a2fe9
OzzkanBulut <ozkanb66@gmail.com>
2022-10-15 22:29:12 +0300

    added exception

--- Numbers/findPItoNthDigit/src/Main.java
+++ Numbers/findPItoNthDigit/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
         Solution solution = new Solution();
-        System.out.println(solution.generatePI(10));
+        System.out.println("Enter a number limit for pi (Limit is 1000) : ");
+        try{
+            int lim = scan.nextInt();
+            System.out.println("PI is : " + solution.generatePI(lim));
+        } catch(Exception e){
+            throw new Exception("Enter a number between 0-1000 !");
+        }
+
     }
 }
